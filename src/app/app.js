4fec22f620da1ecf24a2c8bd6b93021395b3f17d
mornaeldernar <jimenez.rafa@gmail.com>
2022-11-14 21:50:43 -0600

    Estructura inicial de proyecto javascript con docker

---
+++ src/app/app.js
@@ -0,0 +1,9 @@
+const server = http.createServer((req,res) => {
+    res.statusCode = 200
+    res.setHeade('Content-Type','text/plain')
+    res.end('Hello World\n')
+})
+
+server.listen(port,hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`)
+})
