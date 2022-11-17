104576b247ecdc694e451aad6b3714b2c3b191ae
mornaeldernar <jimenez.rafa@gmail.com>
2022-11-16 19:39:20 -0600

    pagina web apache docker

--- src/app/app.js
+++ src/app/app.js
@@ -1,3 +1,7 @@
+var http = required('node:http')
+var port = 8080
+var hostname = '0.0.0.0'
+
 const server = http.createServer((req,res) => {
     res.statusCode = 200
     res.setHeade('Content-Type','text/plain')
